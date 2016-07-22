1c99b2352dea977d0ea9c694cb51dd8af9254fd5
rgalus <biuro@rafalgalus.pl>
2016-07-22 12:00:03 +0200

    Fix module export using AMD, CommonJS, Node.js or global

--- src/sticky.js
+++ src/sticky.js
@@ -150,3 +150,16 @@ class Sticky {
     }
   }
 }
+
+(function(factory) {
+    'use strict';
+
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports !== 'undefined') {
+    module.exports = factory;
+  } else {
+    window.factory;
+  }
+
+}(Sticky));
