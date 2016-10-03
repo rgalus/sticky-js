bdc56ba735d96e9642947583f45b1e03bcdaf986
rgalus <biuro@rafalgalus.pl>
2016-10-03 16:14:08 +0200

    Fix issue with exports conflict between AMD, CommonJS

--- dist/sticky.compile.js
+++ dist/sticky.compile.js
@@ -357,10 +357,10 @@ var Sticky = function () {
 
 
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    define([], factory);
-  } else if (typeof exports !== 'undefined') {
+  if (typeof exports !== 'undefined') {
     module.exports = factory;
+  } else if (typeof define === 'function' && define.amd) {
+    define([], factory);
   } else {
     root.Sticky = factory;
   }
