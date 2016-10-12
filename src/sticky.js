7aa51dc03479b9e23a171c7481b6e0b60ae3cf93
rgalus <biuro@rafalgalus.pl>
2016-10-12 23:46:17 +0200

    Add docs

--- src/sticky.js
+++ src/sticky.js
@@ -12,10 +12,10 @@
 
 class Sticky {
   /**
-   * Sticky instance constructor.
+   * Sticky instance constructor
    * @constructor
-   * @param {string} selector - Selector which we can find elements.
-   * @param {string} options - Global options for sticky elements (could be overwritten by data-{option}="" attributes).
+   * @param {string} selector - Selector which we can find elements
+   * @param {string} options - Global options for sticky elements (could be overwritten by data-{option}="" attributes)
    */
   constructor(selector = '', options = {}) {
     this.selector = selector;
@@ -37,7 +37,7 @@ class Sticky {
 
 
   /**
-   * Activate Sticky library
+   * Function that waits for page to be fully loaded and then renders & activates every sticky element found with specified selector
    * @function
    */
   run() {
@@ -54,12 +54,12 @@ class Sticky {
 
 
   /**
-   * Renders sticky element
+   * Function that assign needed variables for sticky element, that are used in future for calculations and other
    * @function
-   * @param {node} element - Sticky element to be rendered.
+   * @param {node} element - Element to be rendered
    */
   renderElement(element) {
-    // create container where are needed variables are placed
+    // create container for variables needed in future
     element.sticky = {};
 
     // set default variables
@@ -74,7 +74,7 @@ class Sticky {
 
     element.sticky.rect = this.getRectangle(element);
 
-    // fix when el is image that has not yet loaded and width, height = 0
+    // fix when element is image that has not yet loaded and width, height = 0
     if (element.tagName.toLowerCase === 'img') {
       element.onload = () => element.sticky.rect = this.getRectangle(element);
     }
@@ -85,18 +85,18 @@ class Sticky {
 
 
   /**
-   * Activate element if breakpoint
+   * Function that activates element when specified conditions are met and then initalise events
    * @function
-   * @param {node} element - Sticky element to be activated.
+   * @param {node} element - Element to be activated
    */
    activate(element) {
     const heightBefore = element.sticky.container.offsetHeight;
 
-    this.addStyle(element, { position: 'fixed' });
+    this.css(element, { position: 'fixed' });
 
     const heightAfter = element.sticky.container.offsetHeight;
 
-    this.removeStyle(element, ['position']);
+    this.css(element, { position: '' });
 
     if (
       (heightAfter >= heightBefore)
@@ -122,14 +122,22 @@ class Sticky {
    }
 
 
-
+  /**
+   * Function which is adding onResizeEvents to window listener and assigns function to element as resizeListener
+   * @function
+   * @param {node} element - Element for which resize events are initialised
+   */
    initResizeEvents(element) {
     element.sticky.resizeListener = () => this.onResizeEvents(element);
     window.addEventListener('resize', element.sticky.resizeListener);
    }
 
 
-
+  /**
+   * Function which is fired when user resize window. It checks if element should be activated or deactivated and then run setPosition function
+   * @function
+   * @param {node} element - Element for which event function is fired
+   */
    onResizeEvents(element) {
     this.vp = this.getViewportSize();
 
@@ -152,14 +160,22 @@ class Sticky {
    }
 
 
-
+  /**
+   * Function which is adding onScrollEvents to window listener and assigns function to element as scrollListener
+   * @function
+   * @param {node} element - Element for which scroll events are initialised
+   */
    initScrollEvents(element) {
     element.sticky.scrollListener = () => this.onScrollEvents(element);
     window.addEventListener('scroll', element.sticky.scrollListener);
    }
 
 
-
+  /**
+   * Function which is fired when user scroll window. If element is active, function is invoking setPosition function
+   * @function
+   * @param {node} element - Element for which event function is fired
+   */
    onScrollEvents(element) {
     this.scrollTop = this.getScrollTopPosition();
 
@@ -170,13 +186,12 @@ class Sticky {
 
 
   /**
-   * Function
+   * Main function for the library. Here are some condition calculations and css appending for sticky element when user scroll window
    * @function
-   * @param {node} element - Element which sticky container are looked for.
-   * @return {node} element - Sticky container
+   * @param {node} element - Element that will be positioned if it's active
    */
    setPosition(element) {
-    this.removeStyle(element, [ 'position', 'width', 'top', 'left' ]);
+    this.css(element, { position: '', width: '', top: '', left: '' });
 
     if ((this.vp.height < element.sticky.rect.height) || !element.sticky.active) {
       return;
@@ -187,7 +202,7 @@ class Sticky {
     }
 
     if (this.scrollTop > (element.sticky.rect.top - element.sticky.marginTop)) {
-      this.addStyle(element, {
+      this.css(element, {
         position: 'fixed',
         width: element.sticky.rect.width + 'px',
         left: element.sticky.rect.left + 'px',
@@ -200,24 +215,24 @@ class Sticky {
 
         if (element.sticky.stickyClass) element.classList.remove(element.sticky.stickyClass);
 
-        this.addStyle(element, {
+        this.css(element, {
           top: (element.sticky.container.rect.top + element.sticky.container.offsetHeight) - (this.scrollTop + element.sticky.rect.height) + 'px' }
         );
       } else {
         if (element.sticky.stickyClass) element.classList.add(element.sticky.stickyClass);
 
-        this.addStyle(element, { top: element.sticky.marginTop + 'px' });
+        this.css(element, { top: element.sticky.marginTop + 'px' });
       }
     } else {
       if (element.sticky.stickyClass) element.classList.remove(element.sticky.stickyClass);
 
-      this.removeStyle(element, [ 'position', 'width', 'top', 'left' ]);
+      this.css(element, { position: '', width: '', top: '', left: '' });
     }
    }
 
 
   /**
-   * Function that updates element sticky rectangle (with parent container) and then update position;
+   * Function that updates element sticky rectangle (with sticky container), then activate or deactivate element, then update position if it's active
    * @function
    */
    update() {
@@ -232,9 +247,9 @@ class Sticky {
 
 
   /**
-   * Function that returns element in which sticky is stuck (if is not specified, then it is sticky document body);
+   * Function that returns container element in which sticky element is stuck (if is not specified, then it's stuck to body)
    * @function
-   * @param {node} element - Element which sticky container are looked for.
+   * @param {node} element - Element which sticky container are looked for
    * @return {node} element - Sticky container
    */
    getStickyContainer(element) {
@@ -252,21 +267,19 @@ class Sticky {
 
 
   /**
-   * Function that returns element rectangle & position (width, height, top, left).
+   * Function that returns element rectangle & position (width, height, top, left)
    * @function
-   * @param {node} element - Element which position & rectangle are calculated.
+   * @param {node} element - Element which position & rectangle are returned
    * @return {object}
    */
   getRectangle(element) {
-    this.removeStyle(element, [ 'position', 'width', 'top', 'left' ]);
+    this.css(element, { position: '', width: '', top: '', left: '' });
+
+    const width = Math.max(element.offsetWidth, element.clientWidth, element.scrollWidth);
+    const height = Math.max(element.offsetHeight, element.clientHeight, element.scrollHeight);
 
     let top = 0;
     let left = 0;
-    let width = 0;
-    let height = 0;
-
-    width = element.offsetWidth;
-    height = element.offsetHeight;
 
     do {
       top += element.offsetTop || 0;
@@ -279,7 +292,7 @@ class Sticky {
 
 
   /**
-   * Function that returns viewport dimensions in object.
+   * Function that returns viewport dimensions
    * @function
    * @return {object}
    */
@@ -292,7 +305,7 @@ class Sticky {
 
 
   /**
-   * Function that returns scroll top position in pixels.
+   * Function that returns scroll position offset from top
    * @function
    * @return {number}
    */
@@ -302,10 +315,10 @@ class Sticky {
 
 
   /**
-   * Helper function for loops.
+   * Helper function for loops
    * @helper
    * @param {array}
-   * @param {function} callback - Callback function thaht will be invoked to every element from array.
+   * @param {function} callback - Callback function (no need for explanation)
    */
   forEach(array, callback) {
     for (let i = 0, len = array.length; i < len; i++) {
@@ -315,30 +328,17 @@ class Sticky {
 
 
   /**
-   * Helper function to easy add css properties to specified element.
+   * Helper function to add/remove css properties for specified element.
    * @helper
-   * @param {node} element - DOM element.
-   * @param {object} properties - CSS properties that will be added to the specified element.
+   * @param {node} element - DOM element
+   * @param {object} properties - CSS properties that will be added/removed from specified element
    */
-  addStyle(element, properties) {
+  css(element, properties) {
     for (let property in properties) {
       if (properties.hasOwnProperty(property)) {
         element.style[property] = properties[property];
       }
     }
-  }
-
-
-  /**
-   * Helper function to easy remove css properties from specified element.
-   * @helper
-   * @param {node} element - DOM element.
-   * @param {object} properties - CSS properties that will be removed from the specified element.
-   */
-  removeStyle(element, properties) {
-    this.forEach(properties, (property) => {
-      element.style[property] = '';
-    });
   }
 }
 
