91bc5855ebdd41b1fe2df9d7cf2b07e930f58273
rgalus <biuro@rafalgalus.pl>
2016-12-06 20:37:34 +0100

    1.1.6

--- src/sticky.js
+++ src/sticky.js
@@ -3,7 +3,7 @@
  * Sticky.js
  * Library for sticky elements written in vanilla javascript. With this library you can easily set sticky elements on your website. It's also responsive.
  *
- * @version 1.1.5
+ * @version 1.1.6
  * @author Rafal Galus <biuro@rafalgalus.pl>
  * @website https://rgalus.github.io/sticky-js/
  * @repo https://github.com/rgalus/sticky-js
@@ -21,7 +21,7 @@ class Sticky {
     this.selector = selector;
     this.elements = [];
 
-    this.version = '1.1.5';
+    this.version = '1.1.6';
 
     this.vp = this.getViewportSize();
     this.scrollTop = this.getScrollTopPosition();
