ebde97edbbf75bfb991c35cae63d7dbb06970d7f
Rafal Galus <rafal.galus90@gmail.com>
2020-04-29 15:15:49 +0200

    1.3.0

--- dist/sticky.compile.js
+++ dist/sticky.compile.js
@@ -8,7 +8,7 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
  * Sticky.js
  * Library for sticky elements written in vanilla javascript. With this library you can easily set sticky elements on your website. It's also responsive.
  *
- * @version 1.2.2
+ * @version 1.3.0
  * @author Rafal Galus <biuro@rafalgalus.pl>
  * @website https://rgalus.github.io/sticky-js/
  * @repo https://github.com/rgalus/sticky-js
@@ -29,7 +29,7 @@ var Sticky = /*#__PURE__*/function () {
 
     this.selector = selector;
     this.elements = [];
-    this.version = '1.2.2';
+    this.version = '1.3.0';
     this.vp = this.getViewportSize();
     this.body = document.querySelector('body');
     this.options = {
--- src/sticky.js
+++ src/sticky.js
@@ -3,7 +3,7 @@
  * Sticky.js
  * Library for sticky elements written in vanilla javascript. With this library you can easily set sticky elements on your website. It's also responsive.
  *
- * @version 1.2.2
+ * @version 1.3.0
  * @author Rafal Galus <biuro@rafalgalus.pl>
  * @website https://rgalus.github.io/sticky-js/
  * @repo https://github.com/rgalus/sticky-js
@@ -21,7 +21,7 @@ class Sticky {
     this.selector = selector;
     this.elements = [];
 
-    this.version = '1.2.2';
+    this.version = '1.3.0';
 
     this.vp = this.getViewportSize();
     this.body = document.querySelector('body');
